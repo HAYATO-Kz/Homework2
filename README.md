75fb79e2efe2e49eccbb79a8dcef5242814ef18c
Hayato <hayato.k@ku.th>
2018-03-22 02:24:23 +0700

    Done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# Homework2" 
+
+4.1 Explain why this function is so slow, even for fairly small values of n (like n=45 or n=50). 
+
+answer = because fibonacci method will call themselves 2 time in every time that we call method that make it slow when n is very large
--- src/Recursion.java
+++ src/Recursion.java
@@ -1,8 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * @author Hayato Kawai
+ *
+ */
 public class Recursion {
 
+	/**
+	 * Remove duplicate consecutive items from a list, changing the list parameter.
+	 * 
+	 * @param list
+	 *            of any kind of object. The elements are not null.
+	 * @return reference to the list parameter with consecutive duplicates removed.
+	 */
 	public static List unique(List list) {
 		if (list.size() < 2)
 			return list;
@@ -15,10 +27,32 @@ public class Recursion {
 		return list;
 	}
 
-	public static long fibonacci(int n) {
+	public static long oldfibonacci(int n) {
 		if (n < 2)
 			return 1;
-		return fibonacci(n - 1) + fibonacci(n - 2);
+		return oldfibonacci(n - 1) + oldfibonacci(n - 2);
+	}
+
+	public static long newfibonacci(int n) {
+		return newfibonacci2(n, 1, 0);
+	}
+
+	/**
+	 * The method that help fibonacci method work faster than normal fibonacci.
+	 * 
+	 * @param n
+	 *            the position of the value.
+	 * @param next
+	 *            is the next value of fibonacci.
+	 * @param start
+	 *            is the start value.
+	 */
+	public static long newfibonacci2(int n, int next, int start) {
+		if (n == 0)
+			return start;
+		if (n == 1)
+			return next;
+		return newfibonacci2(n - 1, next + start, next);
 	}
 
 	public static void main(String[] args) {
@@ -38,5 +72,14 @@ public class Recursion {
 		// sub.remove(1);
 		// for(Object o: list) System.out.print(o+", ");
 
+		long startTime1 = System.currentTimeMillis();
+		double Recursive1 = oldfibonacci(30);
+		long endTime1 = System.currentTimeMillis();
+		System.out.println("Recursive1 took: " + (endTime1 - startTime1) + " milliseconds");
+
+		long startTime2 = System.currentTimeMillis();
+		double Recursive2 = newfibonacci(17000);
+		long endTime2 = System.currentTimeMillis();
+		System.out.println("Recursive1 took: " + (endTime2 - startTime2) + " milliseconds");
 	}
 }
